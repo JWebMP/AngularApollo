a6136a224f5f1689df10ad15206584b0c7641e6e
GedMarc <ged_marc@hotmail.com>
2026-06-27 01:33:33 +0200

    Jackson migration to version 3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,8 +12,8 @@ module com.jwebmp.angular.graphql {
 
     provides IGuiceScanModuleInclusions with AngularGraphQLScanModule;
 
-    opens com.jwebmp.angular.graphql to com.google.guice, com.jwebmp.core, com.jwebmp.core.angular, com.fasterxml.jackson.databind;
-    opens com.jwebmp.angular.graphql.annotations to com.google.guice, com.jwebmp.core, com.jwebmp.core.angular, com.fasterxml.jackson.databind;
-    opens com.jwebmp.angular.graphql.implementations to com.google.guice, com.jwebmp.core, com.jwebmp.core.angular, com.fasterxml.jackson.databind;
+    opens com.jwebmp.angular.graphql to com.google.guice, com.jwebmp.core, com.jwebmp.core.angular, tools.jackson.databind;
+    opens com.jwebmp.angular.graphql.annotations to com.google.guice, com.jwebmp.core, com.jwebmp.core.angular, tools.jackson.databind;
+    opens com.jwebmp.angular.graphql.implementations to com.google.guice, com.jwebmp.core, com.jwebmp.core.angular, tools.jackson.databind;
 }
 
